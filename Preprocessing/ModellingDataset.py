a844779d911b6befb4e7bd8e9d4b7bc46e068f8b
MariaGraziaMiccoli <miccoli.mariagrazia4@gmail.com>
2023-06-12 10:59:20 +0200

    created datasets for task and added boxplot

--- Preprocessing/ModellingDataset.py
+++ Preprocessing/ModellingDataset.py
@@ -1,30 +1,84 @@
 import pandas as pd
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 class Dataset:
     """
-    classe usata per modellare i dataaset
+    Classe per la modellazione del dataset
     """
 
     def __init__(self, dataset: str):
         """
-        inizializzatore di classe con in input
-        il dataset di cui fare il preprocesssing
+        Costruttore di classe.
+
+        Parametri
+        ----------
+        dataset : nome del dataset su cui fare il preprocessing
+
         """
         self.dataset = pd.read_csv(dataset)
         print(self.dataset.shape)
         print(self.dataset.head(5))
+        #dataFrame = sns.load_dataset(dataset)
+        #sns.heatmap(dataFrame.corr(), annot=True)
 
-    def drop_column(self, column: list):
+    def drop_columns(self, columns: list, file_newDataset: str):
         """
-        Funzione che droppa le feature al momento inutili e
-        verifichiamo che il dataset non contenga dati None
+        Funzione per l'eliminazione delle feature non di nostro interesse
+        e la verifica di ulteriori dati a None
+
+        Parametri
+        ----------
+        columns : lista dei nomi delle colonne da mantenere
+        file_newDataset : stringa contenente il nome che si desidera dare al nuovo file csv
         """
         keys = self.dataset.keys()
-        keys = keys.drop(column)
+        keys = keys.drop(columns)
         self.dataset = self.dataset.drop(keys, axis=1)
         print(self.dataset.shape)
         print(self.dataset.head(5))
-        # verify if there's a none value in dataset
+        # Verifica della presenza di valori None nel dataset
         print(self.dataset.isnull().sum())
+        # Comando usato una volta per splittare il dataset in due parti
+        self.write_csv(file_newDataset)
+
+    def write_csv(self, file_name: str):
+        """
+        Funzione per la creazione di un nuovo file csv
+
+        Parameters
+         ----------
+        file_name : stringa contenente il nome che si desidera dare al file csv
+
+        """
+        rows = self.dataset.to_numpy() #return: list of array for each columns
+
+        df = pd.DataFrame(rows)
+        df.columns = self.dataset.keys()
+        df.to_csv("../Datasets/"+file_name, index=False)
+
+    def valutation_features(self,dataset: str, orientation: str):
+        """
+        Funzione per la valutazione delle features di interesse
+        tramite grafici
+
+        Parameters
+        ----------
+        dataset : stringa contenente il nome del dataset a cui appartengono le features
+        orientation : orientamento del grafico (verticale,orizzontale)
+
+        """
+        #Apply the default theme
+        sns.set_theme()
+        #Load dataset
+        tips = pd.read_csv(dataset)
+        #Create a visualizzation
+        sns.catplot(data=tips, kind="box", orient=orientation)
+        plt.show()
+
+
+
+
+
+
 
--- Preprocessing/__init__.py
+++ Preprocessing/__init__.py
@@ -1,13 +1,15 @@
 from ModellingDataset import Dataset
 
 if __name__ == '__main__':
-    # We use the same Dataset for the two cases of study
-    firstDataset = Dataset('WA_Fn-UseC_-HR-Employee-Attrition.csv')
-    secondDataset = Dataset('WA_Fn-UseC_-HR-Employee-Attrition.csv')
+    # Verrà usato lo stesso dataset per due diversi task
+    firstDataset = Dataset('../Datasets/WA_Fn-UseC_-HR-Employee-Attrition.csv')
+    secondDataset = Dataset('../Datasets/WA_Fn-UseC_-HR-Employee-Attrition.csv')
 
+    # Features da mantere per il primo task
     featureDropFirstDataset = ['Age', 'BusinessTravel', 'EducationField', 'Gender',
                                'JobLevel', 'JobRole', 'NumCompaniesWorked']
 
+    # Features da mantenere per il secondo task
     featureDropSecondDataset = ['BusinessTravel', 'DailyRate', 'DistanceFromHome',
                                 'HourlyRate', 'JobInvolvement', 'JobSatisfaction',
                                 'MaritalStatus', 'MonthlyIncome', 'MonthlyRate', 'OverTime',
@@ -15,5 +17,15 @@ if __name__ == '__main__':
                                 'WorkLifeBalance', 'YearsAtCompany', 'YearsInCurrentRole',
                                 'YearsSinceLastPromotion', 'YearsWithCurrManager']
 
-    firstDataset.drop_column(featureDropFirstDataset)
-    secondDataset.drop_column(featureDropSecondDataset)
+    # Drop delle features che non servono
+    #firstDataset.drop_columns(featureDropFirstDataset, "First_dataset.csv")
+    #secondDataset.drop_columns(featureDropSecondDataset, "Second_dataset.csv")
+
+    # Generazione dei grafici relativi alla valutazione delle features
+    firstDataset.valutation_features('../Datasets/First_dataset.csv', "v")
+    secondDataset.valutation_features('../Datasets/Second_dataset.csv', "h")
+
+    #NOTE DA RIMUOVERE
+    #TO DO:
+    #Overrated in: MontlyInCome --> min 1009 max 20.0k
+    #              NumCompaniesWorked --> min 0 max 9
